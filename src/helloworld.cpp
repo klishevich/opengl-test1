d916650853c1aa5fb8d4b38f6b2376ea8e9852e9
Michael Klishevich <busation@gmail.com>
2020-01-10 00:15:12 +0300

    hello window part 1

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,52 +1,47 @@
-/* Ask for an OpenGL Core Context */
-#define GLFW_INCLUDE_GLCOREARB
-#include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
-#define BUFFER_OFFSET(i) ((char *)NULL + (i))
-
-int main(int argc, char **argv)
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
-    GLFWwindow *window;
+    glViewport(0, 0, width, height);
+}
 
-    /* Initialize the library */
-    if (!glfwInit())
-    {
-        return -1;
-    }
-
-#ifdef __APPLE__
-    /* We need to explicitly ask for a 3.2 context on OS X */
+int main()
+{
+    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#endif
+    // For Mac
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
-    if (!window)
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the buffers
-
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
     glfwTerminate();
+
     return 0;
 }
